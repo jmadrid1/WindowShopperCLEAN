6fa0da835085d9e1137a7edde1c784c281a8925d
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2022-02-15 11:37:06 -0800

    Renamed FirebaseCart Repo and Implementation
    Renamed FirebaseCart Repo and Implementation

--- app/src/main/java/com/example/windowshopperclean/features/shop/domain/use_cases/AddToCart.kt
+++ app/src/main/java/com/example/windowshopperclean/features/shop/domain/use_cases/AddToCart.kt
@@ -1,11 +1,11 @@
 package com.example.windowshopperclean.features.shop.domain.use_cases
 
 import com.example.windowshopperclean.features.shop.data.CartItem
-import com.example.windowshopperclean.features.shop.data.remote.FirebaseShopRepo
+import com.example.windowshopperclean.features.shop.data.remote.FirebaseShopRepoImpl
 import javax.inject.Inject
 
-class AddToCart @Inject constructor(private val _firebase: FirebaseShopRepo) {
+class AddToCart @Inject constructor(private val _firebaseImpl: FirebaseShopRepoImpl) {
 
-    suspend operator fun invoke(cartItem: CartItem, uid: String) = _firebase.addToCart(cartItem, uid)
+    suspend operator fun invoke(cartItem: CartItem, uid: String) = _firebaseImpl.addToCart(cartItem, uid)
 
 }
