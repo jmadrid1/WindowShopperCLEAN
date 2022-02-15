88b839df0d0320602668bea4a654b519a3d2d63a
jmadrid1 <31678109+jmadrid1@users.noreply.github.com>
2022-02-15 11:37:22 -0800

    Renamed Firebase Repos and Implementations
    Renamed Firebase Repos and Implementations

--- app/src/main/java/com/example/windowshopperclean/di/AppModule.kt
+++ app/src/main/java/com/example/windowshopperclean/di/AppModule.kt
@@ -4,13 +4,13 @@ import com.example.windowshopperclean.features.account.data.remote.FirebaseAccou
 import com.example.windowshopperclean.features.account.domain.use_cases.AddUserAccount
 import com.example.windowshopperclean.features.account.domain.use_cases.GetAccountData
 import com.example.windowshopperclean.features.account.domain.use_cases.UpdateEmailAddress
-import com.example.windowshopperclean.features.cart.data.remote.FirebaseCartRepo
+import com.example.windowshopperclean.features.cart.data.remote.FirebaseCartRepoImpl
 import com.example.windowshopperclean.features.cart.domain.use_cases.GetCartItems
 import com.example.windowshopperclean.features.cart.domain.use_cases.RemoveItemFromCart
-import com.example.windowshopperclean.features.reviews.domain.repository.FirebaseReviewsRepo
+import com.example.windowshopperclean.features.reviews.domain.repository.FirebaseReviewsRepoImpl
 import com.example.windowshopperclean.features.reviews.domain.use_cases.GetReviews
 import com.example.windowshopperclean.features.reviews.domain.use_cases.SubmitReview
-import com.example.windowshopperclean.features.shop.data.remote.FirebaseShopRepo
+import com.example.windowshopperclean.features.shop.data.remote.FirebaseShopRepoImpl
 import com.example.windowshopperclean.features.shop.domain.use_cases.GetClothes
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -51,31 +51,31 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideGetClothesUseCase(repository: FirebaseShopRepo): GetClothes {
+    fun provideGetClothesUseCase(repository: FirebaseShopRepoImpl): GetClothes {
         return GetClothes(repository)
     }
 
     @Provides
     @Singleton
-    fun provideGetReviewsUseCase(repository: FirebaseReviewsRepo): GetReviews {
+    fun provideGetReviewsUseCase(repository: FirebaseReviewsRepoImpl): GetReviews {
         return GetReviews(repository)
     }
 
     @Provides
     @Singleton
-    fun provideSubmitReviewUseCase(repository: FirebaseReviewsRepo): SubmitReview {
+    fun provideSubmitReviewUseCase(repository: FirebaseReviewsRepoImpl): SubmitReview {
         return SubmitReview(repository)
     }
 
     @Provides
     @Singleton
-    fun provideGetCartItemsUseCase(repository: FirebaseCartRepo): GetCartItems {
+    fun provideGetCartItemsUseCase(repository: FirebaseCartRepoImpl): GetCartItems {
         return GetCartItems(repository)
     }
 
     @Provides
     @Singleton
-    fun provideRemoveItemFromCartUseCase(repository: FirebaseCartRepo): RemoveItemFromCart {
+    fun provideRemoveItemFromCartUseCase(repository: FirebaseCartRepoImpl): RemoveItemFromCart {
         return RemoveItemFromCart(repository)
     }
 
